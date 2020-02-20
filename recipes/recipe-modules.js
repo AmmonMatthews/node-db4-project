ff5fa8d66639bd9c097aefdc36fe66155e1aca19
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2020-02-19 20:54:07 -0700

    mvp and some stretch

--- recipes/recipe-modules.js
+++ recipes/recipe-modules.js
@@ -3,7 +3,7 @@ const db = require('../data/dbConfig.js')
 module.exports = {
     getRecipes,
     getShoppingList,
-    // getInstructions
+    getInstructions
 }
 
 function getRecipes(){
@@ -12,9 +12,14 @@ function getRecipes(){
 
 function getShoppingList(id){
     return db('recipe_ingredients as ri')
-    .join('ingredients as i', 'ri.ingredients_id', 'i.Id' )
-    .join('recipes as r', 'ri.recipe_id', 'r.Id')
-    .select('r.recipe_name', 'i.ingredients_name', 'ri.ingredients_quantity')
-    .where("recipe_id", id)
+    .join('recipes as r', 'ri.recipe_id', 'r.id')
+    .join('ingredients as i', 'ri.ingredients_id', 'i.id' )
+    .select('r.recipe_name', 'i.ingredient_name', 'ri.ingredient_quantity')
+    .where("ri.recipe_id", id)
+}
 
+function getInstructions(id){
+    return db('recipes as r')
+    .select('r.recipe_name as RecipeName','r.description as Instructions')
+    .where('r.id', id)
 }
--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -3,7 +3,7 @@ const express = require('express')
 const Recipes = require('./recipe-modules.js')
 
 const router = express.Router();
-
+//Tested
 router.get('/', (req, res) => {
     Recipes.getRecipes()
     .then(recipes =>{
@@ -14,6 +14,30 @@ router.get('/', (req, res) => {
         res.status(404).json({error:"cannot find recipes"})
     })
 })
+//Tested
+router.get('/:id/shoppingList', (req, res) =>{
+    Recipes.getShoppingList(req.params.id)
+    .then(list => {
+        res.status(200).json(list)
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(404).json({error:"cannot find shopping list"})
+    })
+})
+
+router.get('/:id/instructions', (req, res) => {
+    Recipes.getInstructions(req.params.id)
+    .then(instruct => {
+        res.status(200).json(instruct);
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(404).json({error:"cannot find instructions"})
+    })
+
+})
+
 
 
 
